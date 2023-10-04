9b16632f0f978181535a162eb1a3d7a379678781
reyunj <junrey1296@gmail.com>
2023-10-04 10:54:43 +0800

    update-footer responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,18 +8,19 @@ import footer from '@/public/footer.png'
 const Footer = () => {
   return (
     <footer>
-        <div className="bg-gradient-to-b from-blue-700 to-transparent bg-blend-multiply py-16">
-            <div className="flex justify-between text-white px-80">
+      <div className="bg-gradient-to-b from-blue-700 to-transparent bg-blend-multiply text-white">
+        <div className="container mx-auto">
+          <div  className="flex flex-col md:flex-row md:justify-between items-center gap-y-5 py-16">
                 <div>
-                    <h4 className="pb-4 font-bold">Get Started</h4>
+                <h4 className="pb-4 font-bold">Get Started</h4>
                     <ul>
                         <li className="pb-2">Login</li>
                         <li className="pb-2">Join Our Group</li>
                         <li className="pb-2">Visit Our Group</li>
                         <li className="pb-2">30 Day Trial Membership</li>
                     </ul>
-                 </div>
-                  <div>
+                </div>
+                <div>
                     <h4 className="pb-4 font-bold">About</h4>
                     <ul>
                         <li className="pb-2">Why Lorem Ipsum?</li>
@@ -31,26 +32,31 @@ const Footer = () => {
                 <div>
                     <h4 className="pb-4 font-bold">Contact Us</h4>
                     <ul>
-                        <li className="pb-2">4517 Washington Ave. Manchester, Kentucky 39495</li>
+                        <li className="pb-2">4517 Washington Ave.</li>
+                        <li> Manchester, Kentucky 39495</li>
                         <li className="pb-2">(555) 543 - 7653</li>
+                        <li>.</li>
                     </ul>
                 </div>
                 <div>
                     <Image src={footer} alt= 'footer'/>
                 </div>
+          </div>
         </div>
+      </div>
+      <div  className="bg-blue-900 text-white">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row md:justify-between items-center gap-y-5 py-16">
+              <p className="text-center text-sm pb-3">Copyright © 2023 Lorem Ipsum. All rights reserved. | AGDP IT Solutions</p>
+              <div className="flex gap-x-5 items-center">
+                <Image src={linkedin} alt="linkedin" />
+                <Image src={fb} alt="fb" />
+                <Image src={twitter} alt="twitter" />
+                <Image src={youtube} alt="youtube" />
+              </div>
         </div>
-        <div className="bg-blue-900 text-white py-5">
-        <div className="px-80 flex items-center">
-                Copyright © 2023 Lorem Ipsum. All rights reserved. | AGDP IT Solutions
-             </div>
-          <div className="px-80 flex justify-end items-center h-full gap-x-5">
-            <Image src={linkedin} alt="linkedin" />
-          <Image src={fb} alt="fb" />
-          <Image src={twitter} alt="twitter" />
-          <Image src={youtube} alt="youtube" />
-        </div>
-        </div>
+      </div>
+    </div>
     </footer>
   )
 }
